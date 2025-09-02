29b26718b4025bae071b5e321aef2df58af268d2
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-09-02 22:14:45 +0530

    imroved login system
    - login form is only visible after login button is pressed
    - users can cancel login by pressing cancel button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,10 @@ import Notification from './components/Notification'
 import noteService from './services/notes'
 import Note from './components/Note'
 import loginService from './services/login'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
+  const [loginVisible, setLoginVisible] = useState(false)
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
@@ -97,31 +99,28 @@ const App = () => {
     }
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
+  const loginForm = () => {
+    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+    const showWhenVisible = { display: loginVisible ? '' : 'none' }
+
+    return (
       <div>
-        <label>
-          username
-          <input 
-            type='text'
-            value={username}
-            onChange={({target}) => setUsername(target.value)}
+        <div style={hideWhenVisible}>
+          <button onClick={() => setLoginVisible(true)}>log in</button>
+        </div>
+        <div style={showWhenVisible}>
+          <LoginForm 
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
           />
-        </label>
+          <button onClick={() => setLoginVisible(false)}>cancel</button>
+        </div>
       </div>
-      <div>
-        <label>
-          password
-          <input
-            type='password'
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </label>
-      </div>
-      <button type='submit'>login</button>
-    </form>
-  )
+    )
+  }
 
   const noteForm = () => (
     <form onSubmit={addNote}>
