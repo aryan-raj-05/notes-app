cb60386c51c5e8920768070053cf0e6135e7e88e
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-09-25 21:04:08 +0530

    wrote tests for react frontend

--- client/src/components/NoteForm.test.jsx
+++ client/src/components/NoteForm.test.jsx
@@ -4,16 +4,16 @@ import userEvent from '@testing-library/user-event'
 
 test('<NoteForm /> updates parent state and calls onSubmit', async () => {
   const createNote = vi.fn()
-  const user = userEvent.setup()
 
   render(<NoteForm createNote={createNote} />)
 
-
-  const input = screen.getByRole('textbox')
+  const input = screen.getByPlaceholderText('write note content here')
   const sendButton = screen.getByText('save')
 
-  await user.type(input, 'testing a form...')
-  await user.click(sendButton)
+  await userEvent.type(input, 'testing a form...')
+  await userEvent.click(sendButton)
+
+  console.log(createNote.mock.calls)
 
   expect(createNote.mock.calls).toHaveLength(1)
   expect(createNote.mock.calls[0][0].content).toBe('testing a form...')
